78077a99842acec43ad2930c70c4d3d554ff893b
Elianarve <elirve2@outlook.es>
2024-10-02 13:12:55 +0200

    added-folders

---
+++ server/models/userModel.js
@@ -0,0 +1,28 @@
+import { DataTypes } from "sequelize";
+import connection_db from "../database/connection_db.js";
+
+const UsersModel = connection_db.define('users', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+},{
+    tableName: 'users',
+    timestamps: false
+});
+
+export default UsersModel;
