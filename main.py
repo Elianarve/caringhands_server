c402fc8fdbba79aec88de928e5c1f7f1ee4f52ee
joserodr68 <joserodr68@gmail.com>
2024-10-03 11:50:54 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,37 +1,38 @@
 
 from langchain.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
-
-
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.schema import StrOutputParser
 from langchain.schema.runnable import RunnablePassthrough
 from langchain_community.vectorstores import Chroma
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+import os
+from dotenv import load_dotenv
 
 
 def query_groq(user_input):
-    # Set API key for LLM (replace this with environment variable in production)
-    api_key = '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'
+    
+    load_dotenv()
+    api_key = os.getenv("GROQ_API_KEY")
 
-    # Initialize the language model
+    # Inicializamos el LLM
     llm = ChatGroq(
         temperature=0,
         api_key=api_key,
         model_name='llama-3.1-70b-versatile'
     )
 
-    # Load embeddings and Chroma vector store
+    # Cargamos embeddings y la vector database Chroma. La construcción de la base de datos se ha realizado previamente
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     persist_directory = "./chroma_db"
     vectorstore = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
 
-    # Create retriever from Chroma vector store
+    # Creamos un retriever para Chroma
     retriever = vectorstore.as_retriever(search_type="mmr", search_kwargs={"k": 5, "fetch_k": 20})
 
-    # Define the system and human messages
+    # Mensajes system y human messages
     system_template = """ 
     You are a medical expert. You will be asked a question in Spanish, translate precisely to English. 
     Answer the question based on the following context: {context}.
@@ -41,12 +42,13 @@ def query_groq(user_input):
     Question: {question}
     Answer:
     """
+    # RAG con documentos relevantes de PUBMED, referencia en el sector médico
     
-    # Initialize prompt with system and human message
+    # Creamos el prompt
     prompt = ChatPromptTemplate.from_template(system_template)
 
     
-    # Define RAG chain using retriever and prompt
+    #  RAG chain 
     rag_chain = (
         {
             "context": retriever, 
@@ -57,7 +59,7 @@ def query_groq(user_input):
         | StrOutputParser()
     )
 
-    # Function to handle predictions with RAG
+    # Respuesta del LLM con  RAG
     def predict(input_text):
         response = rag_chain.invoke(input_text)
         return response
@@ -85,7 +87,5 @@ async def answer_question(q: Question):
 
 
 # Ejecutar la aplicación usando Uvicorn:
-# Iniciar el servidor desde la terminal con el siguiente comando:
 # uvicorn main:app --reload
-# Con la aplicación corriendo, ir al navegador e introducir: http://127.0.0.1:8000/docs. Ahi interectuamos con la API
 
